6e5a87e85574776e2cf8450a1dd363691b37d78b
xu han <xhan87@gmail.com>
2011-04-04 00:40:07 +0800

    first demo

--- CrazyText/AppDelegate.rb
+++ CrazyText/AppDelegate.rb
@@ -7,9 +7,91 @@
 #
 
 class AppDelegate
-    attr_accessor :window
+    attr_accessor :window, :tf_source, :tf_result
+    attr_accessor :convertAry, :btnCurEffect
+    
+    def initialize
+      @convertAry = []
+      @convertAry.push(
+                {"name" => "Plain", "method" => "plain"},      
+                {"name" => "Bubble", "method" => "bubble"},
+                {"name" => "Upsidedown", "method" => "up_down" },
+                {"name" => "Slash", "method" => "slash"},
+                {"name" => "Greek", "method" => "geek" },
+                {"name" => "Russian", "method" => "russian"},
+                {"name" => "Weird", "method" => "weird"},
+                {"name" => "Japan", "method" => "japan"},
+                {"name" => "Medieval", "method" => "medieval"},
+                {"name" => "Dots", "method" => "dots"},
+                {"name" => "Tails", "method" => "tails"},
+                {"name" => "Accents", "method" => "accents"},
+                {"name" => "Number", "method" => "number"},                
+                {"name" => "Line", "method" => "line"},
+                {"name" => "Thailand", "method" => "thailand" },
+                {"name" => "Symbols", "method" => "symbol"},
+                {"name" => "Wacky", "method" => "wacky"},
+                {"name" => "Reverse", "method" => "reverse"},
+                {"name" => "ReverseWord", "method" => "reverse_word"},
+                {"name" => "WOW", "method" => "juhua"}
+        )
+      @selected_effect = @convertAry[0]
+      
+    end
+    
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+        @converter = TextConverter.new
+        @isExpand = false
+        triggle_effects(@isExpand,false)
+        setEffectByName(@selected_effect["name"])        
+    end
+    
+    
+    def triggle_effects(_isExpand,isAnimation)
+       # height =  _isExpand ? 575 : 575 -230
+      newFrame = @window.frame
+      newFrame.size.height += _isExpand ? 230 : -230
+      newFrame.origin.y -= _isExpand ? 230 : -230
+      @window.setFrame(newFrame,:display => true, :animate => isAnimation)
+    end
+    
+    def triggle_effect(sender)
+      @isExpand = !@isExpand
+      triggle_effects(@isExpand,true)
+    end
+    
+    def controlTextDidChange(a_notification)
+        str = tf_source.stringValue
+        
+        # result = @converter.juhua(str)
+        # result = @converter.reverse(str)
+        #        result = @converter.slash(str)
+        #        puts result
+        
+        result = @converter.slash(str)
+        tf_result.setStringValue(result)
+        # tf_result.setStringValue(str)
+    end
+    
+    def textDidChange(a_notification)
+      str = tf_source.textStorage.string
+      result = @converter.run(@selected_effect["method"], str)
+      tf_result.setStringValue(result)
+    end
+    
+    def onEffectChanged(sender)
+      title = sender.title
+      setEffectByName(title)
+    end
+    
+    def setEffectByName name  
+      value = @convertAry.select { |hash| hash["name"] == name }
+      if value.count >= 1
+        @selected_effect = value.first
+        btnCurEffect.setTitle(@selected_effect["name"])
+        textDidChange(nil)        
+      end
+
     end
 end
 
---
+++ CrazyText/TextConverter.rb
@@ -0,0 +1,146 @@
+#
+#  TextConverter.rb
+#  CrazyText
+#
+#  Created by xhan on 3/31/11.
+#  Copyright 2011 xu han. All rights reserved.
+#
+
+# scan(/./m) means each world(UTF8) codes from jcode
+
+class TextConverter
+  
+  def initialize
+    @abc52 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    @ary_abc = @abc52.scan(/./m)
+  end
+  
+  def run(methodName,string)
+    instance_eval("#{methodName}(%{#{string}})")
+  end
+  
+  def plain(str)
+    str
+  end
+  
+  def juhua(str)    
+    str.gsub(/\B/m,"\322\211")
+     # str.scan(/./m).map{ |char| char == " "[0] ? char : "\322#{char}\211"}.join
+  end
+  
+  def reverse(str)
+      str.scan(/./m).reverse.join  
+  end
+  
+  def reverse_word(str)
+    str.scan(/\w+/m).reverse.join(' ')
+  end
+  
+  def reverse_ii(str)    
+    "\342\200\256"<<str    
+  end
+
+  def test(str)
+    str.gsub(/\B/m,"̸")
+  end
+
+
+  def up_down(str)
+      table = {}
+      foo = "abcdefghijklmnopqrstuvwxyz:;,.!?"
+      bar = "ɐqɔpǝɟƃɥıɾʞlɯuodbɹsʇnʌʍxʎz:؛'˙¡¿"
+      arr_foo = foo.scan(/./m)
+      arr_bar = bar.scan(/./m)
+      (0..31).each do |i| 
+        table[arr_foo[i]] = arr_bar[i]
+      end
+      str.scan(/./m).map{|c| table[c] ? table[c] : c}.join
+      
+  end
+
+
+#  crazy text
+  def bubble(str)
+    convert_by_abc_string(str,"ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ")
+  end
+  
+  def geek(str)
+    convert_by_abc_string(str,"ΔβCDΣҒGHIJKLMΠΩPQRSTU∇ШXΨZΔβCDΣҒGHIJKLMΠΩPQRSTU∇ШXΨZ")
+  end
+  
+  def slash(str)
+    abc_key = "a̸b̸c̸d̸e̸f̸g̸h̸i̸j̸k̸l̸m̸n̸o̸p̸q̸r̸s̸t̸u̸v̸w̸x̸y̸z̸A̸B̸C̸D̸E̸F̸G̸H̸I̸J̸K̸L̸M̸N̸O̸P̸Q̸R̸S̸T̸U̸V̸W̸X̸Y̸Z̸"
+    table = {}
+    array_goal = abc_key.scan(/../m)
+     return  nil if array_goal.count != 26*2
+    
+    (0..25).each do |i|
+      table[@ary_abc[i]] = array_goal[i]
+    end
+    str.scan(/./m).map{|c| table[c] ? table[c] : c}.join    
+  end
+  
+  def russian(str)
+    convert_by_abc_string(str,"ДБCDΞFGHIJҜLMИФPǪЯSΓЦVЩЖУZДБCDΞFGHIJҜLMИФPǪЯSΓЦVЩЖУZ")
+  end
+  
+  def weird(str)
+    convert_by_abc_string(str,"ɑБϲժҽƒցհíյƘӀʍղօԹզɾՏԵմѵա×վՀɑБϲժҽƒցհíյƘӀʍղօԹզɾՏԵմѵա×վՀ")
+  end
+  
+  def japan(str)
+    convert_by_abc_string(str,"丹乃匚刀モ下ム卄工ＪＫㄥ爪れ◯ㄗＱ尺ち匕∪∨山メㄚ乙丹乃匚刀モ下ム卄工ＪＫㄥ爪れ◯ㄗＱ尺ち匕∪∨山メㄚ乙")
+  end
+  
+  def medieval(str)
+    convert_by_abc_string(str,"λB₡ÐE₣GҤłJƘŁM₦ØPQƦ$ŦUV₩X¥ZλB₡ÐE₣GҤłJƘŁM₦ØPQƦ$ŦUV₩X¥Z")
+  end
+  
+  def dots(str)
+    # str.gsub(/\B/m,"̤̈")
+    # version 2 , 32 = space
+    str.scan(/./m).map{ |char| char == " "[0] ? char : "#{char}̤̈"}.join
+  end
+  
+  def tails(str)
+    convert_by_abc_string(str,"ąɓçdęƒɠђįʝķɭɱŋǫƥʠŗşţųvwҳƴʐĄβÇDĘƑĢҢĮJĶĻMƝǪƤQŖŞŢŲVWҲҶζ")
+  end
+  
+  def accents(str)
+    convert_by_abc_string(str,"ÁßČĎĔŦĞĤĨĴĶĹMŃŐPQŔŚŤÚVŴЖŶŹÁßČĎĔŦĞĤĨĴĶĹMŃŐPQŔŚŤÚVŴЖŶŹ")
+  end
+  
+  def number(str)
+    convert_by_abc_string(str,"48(d3f9h!jk1mn0pqr57uvwxy248(d3f9h!jk1mn0pqr57uvwxy2")
+  end
+  
+  def line(str)
+    # str.gsub(/\B/m,"̲̅")
+    str.scan(/./m).map{ |char| char == " "[0] ? char : "#{char}̲̅"}.join
+  end
+  
+  def thailand(str)
+    convert_by_abc_string(str,"ค๒ς๔єŦɠђเןкl๓ภ๏ԹợгรԵยשฬאվzค๒ς๔єŦɠђเןкl๓ภ๏ԹợгรԵยשฬאվz")
+  end
+  
+  def symbol(str)
+    convert_by_abc_string(str,"⋀♇☪☭☰╒ⓖ♅¡♩⊀↳♍♌☻☟φ®$☂☋▽♆✄¥☡⋀♇☪☭☰╒ⓖ♅¡♩⊀↳♍♌☻☟φ®$☂☋▽♆✄¥☡")
+  end
+  
+  def wacky(str)
+    convert_by_abc_string(str,"ąßcđ૯FGસIJKLMர૭PQરş┼ચVЩﾒY乙ąßcđ૯FGસIJKLMர૭PQરş┼ચVЩﾒY乙")
+  end
+  
+  private
+  def convert_by_abc_string(origin,abc_key)
+    table = {}
+    array_goal = abc_key.scan(/./m)
+     return  nil if array_goal.count != 26*2
+    
+    (0..25).each do |i|
+      table[@ary_abc[i]] = array_goal[i]
+    end
+    origin.scan(/./m).map{|c| table[c] ? table[c] : c}.join
+  end
+  
+end
